9a384fceb0cacf8aa3de79a1d88b7733819c634e
Mayu Koresawa <e175770@ie.u-ryukyu.ac.jp>
2019-12-24 04:49:09 +0900

    step3 done

--- src/main/java/jp/ac/uryukyu/ie/e175770/LivingThing.java
+++ src/main/java/jp/ac/uryukyu/ie/e175770/LivingThing.java
@@ -6,6 +6,13 @@ public class LivingThing {
     private int attack;
     private boolean dead;
 
+    /**
+     *コンストラクタ
+     * @param name:名前
+     * @param maximumHP:最大HP
+     * @param attack:攻撃力
+     */
+
     public LivingThing(String name, int maximumHP, int attack) {
         this.name = name;
         hitPoint = maximumHP;
@@ -14,10 +21,18 @@ public class LivingThing {
         System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
     }
 
+    /**
+     * 死んでいるか判断する
+     * @return　trueなら死亡
+     */
     public boolean isDead() {
         return dead;
     }
 
+    /**
+     * ゲッターメソッド
+     * @return　名前
+     */
     public String getName() {
         return name;
     }
@@ -32,14 +47,26 @@ public class LivingThing {
         }
     }
 
+    /**
+     * hitpointのゲッター
+     * @return　HP
+     */
     public int getHitPoint() {
         return hitPoint;
     }
 
+    /**
+     *hitpointのセッター
+     * @param hitPoint
+     */
     public void setHitPoint(int hitPoint) {
         this.hitPoint = hitPoint;
     }
 
+    /**
+     * attackのゲッター
+     * @return
+     */
     public  int getAttack(){
         return attack;
     }
@@ -48,13 +75,26 @@ public class LivingThing {
         this.attack = attack;
     }
 
+    /**
+     * deadのゲッター
+     * @return
+     */
     public boolean getDead(){
         return dead;
     }
 
+    /**
+     * deadのセッター
+     * @param dead:生死状態
+     */
     public void setDead(boolean dead){
         this.dead = dead;
     }
+
+    /**
+     * 自分へのダメージを計算するメソッド
+     * @param damage:ダメージ
+     */
     public void wounded(int damage) {
         hitPoint -= damage;
         if (hitPoint < 0) {
